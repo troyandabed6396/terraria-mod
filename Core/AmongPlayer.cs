4da6ac27ebc1492e4c5600a8dca6fc91337f093f
Armaan Priadarshan <43357808+armaanpriyadarshan@users.noreply.github.com>
2022-04-19 12:27:39 -0400

    a

--- Core/AmongPlayer.cs
+++ Core/AmongPlayer.cs
@@ -4,14 +4,5 @@ using Terraria.ModLoader;
 using TestMod.Core.Buffs;
 
 namespace TestMod.Core {
-    public class AmongPlayer : ModPlayer {
-        public bool wokked = false;
-        public void isWokked() {
-            if (wokked) {
-                player.AddBuff(ModContent.BuffType<WOKKED>(), -1);
-            } else if (player.HasBuff(ModContent.BuffType<WOKKED>()) && !wokked) {
-                player.DelBuff(ModContent.BuffType<WOKKED>());
-            }
-        }
-    }
+    public class partial AmongPlayer : ModPlayer {}
 }
